31809d3bdaf37647060b12c2f02109873912e347
Sailesh Rijal <saileshrijal1@gmail.com>
2023-01-21 20:48:51 +0545

    db connection added

--- Notes.Api/Program.cs
+++ Notes.Api/Program.cs
@@ -1,15 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Notes.Data;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options=> options.UseSqlServer(connectionString));
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
---
+++ Notes.Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option):base(option)
+        {
+
+        }
+    }
+}
