c494f861143e9f0ec28af5efda2b28b909087777
Abhishek Bohora <abhishek.bohora16@gmail.com>
2023-01-22 14:09:56 +0545

    note controller added

---
+++ Notes.Api/Controllers/NoteController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Notes.Models;
+using Notes.Services.interfaces;
+using Notes.ViewModels;
+
+namespace Notes.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoteController : ControllerBase
+    {
+        private readonly INoteService _noteService;
+        public NoteController(INoteService  noteService)
+        {
+              _noteService= noteService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll() {
+            var notes = await _noteService.GetAllNotes();
+            var notesListVm = notes.Select(x => new NoteVM() {
+                Id= x.Id,
+                Title= x.Title,
+                Description= x.Description,
+                CategoryId= x.CategoryId,
+                CreatedOn   = x.CreatedOn,
+            }).ToList();
+
+            return Ok(notesListVm); 
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetById(int id) {
+            var note = await _noteService.GetNoteById(id);
+            if (note == null) { return BadRequest("Notes could not be found"); }
+
+            var notesListVm = new NoteVM() {
+               Id = note.Id,
+               Title= note.Title,
+               Description= note.Description,
+               CategoryId= note.CategoryId,
+               CreatedOn = note.CreatedOn,
+            };
+
+            return Ok(notesListVm); 
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(NoteVM vm) {
+            if(!ModelState.IsValid) { return BadRequest("some fields are missing"); }
+            try
+            {
+                var note = new Note {
+                    Id = vm.Id,
+                    Title= vm.Title,
+                    Description= vm.Description,
+                    CategoryId= vm.CategoryId,
+                    CreatedOn = vm.CreatedOn,
+                       
+                };
+                var result = _noteService.AddNotes(note);
+                if (result != null)
+                {
+                    return Ok("Notes created successfully");
+                }
+                return BadRequest("Notes cannot be added");
+            }catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(int id, NoteVM vm)
+        {
+            if (!ModelState.IsValid) { return BadRequest("Some fields are missing"); }
+            if(id != vm.Id) { return BadRequest("Id do not match"); }
+            try
+            {
+                var existingNotes = await _noteService.GetNoteById(id);
+                if (existingNotes == null) { return BadRequest("Notes with the id is not found"); }
+                var note = new Note
+                {
+                    Id = vm.Id,
+                    Title = vm.Title,
+                    Description = vm.Description,
+                    CategoryId = vm.CategoryId,
+                    CreatedOn = vm.CreatedOn,
+                };
+
+                var result = await _noteService.UpdateNotes(note);
+                if (result) {
+                    return Ok("Note updated successfully");
+                }else{
+                    return BadRequest("Note cannot be updated");
+                }
+            }
+            catch(Exception ex) { return BadRequest(ex.Message); }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var existingNote = await _noteService.GetNoteById(id);
+            if (existingNote == null) { return BadRequest("Notes cannot be found with the id: "+id); }
+            var result = await _noteService.DeleteNotes(id);
+            if (result) { return Ok("Note deleted successfully"); }
+            return BadRequest("Note cannot be deleted");
+        }
+    }
+}
---
+++ Notes.ViewModels/NoteVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Notes.ViewModels
+{
+    public class NoteVM
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Title { get; set; }
+        [Required]
+        public string? Description { get; set; }
+        public int CategoryId { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+    }
+}
